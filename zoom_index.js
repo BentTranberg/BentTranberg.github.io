a4c5b197b3dc04bdc9f9b6d60a34e4e2a27b0d59
Bent Tranberg <bent.tranberg@gmail.com>
2017-03-18 14:54:17 +0100

    Publishing test
    First output from authoring tool.

---
+++ zoom_index.js
@@ -0,0 +1,74 @@
+dictwords = ["first 0 39 232",
+	"topic 0 39 216 1 39 216 4 39 216 5 39 216",
+	"click 0 10 16 1 10 16 2 10 16 3 10 16 4 10 16 5 10 16",
+	"display 0 10 16 1 10 16 2 10 16 3 10 16 4 10 16 5 10 16",
+	"table 0 10 16 1 10 16 2 10 16 3 10 16 4 10 16 5 10 16",
+	"contents 0 10 16 1 10 16 2 10 16 3 10 16 4 10 16 5 10 16",
+	"navigation 0 10 8 1 10 8 2 10 8 3 10 8 4 10 8 5 10 8",
+	"introduction 0 16 136 2 39 214 4 16 136",
+	"this 0 15 72 1 15 72 2 23 74 3 23 74 4 15 72 5 15 72",
+	"the 0 15 72 1 15 72 2 15 66 4 15 72 5 15 72",
+	"first-topic.html 0 6 64",
+	"fourth 1 39 216",
+	"more 1 16 136 3 39 230 5 16 136",
+	"stuff 1 16 136 3 39 214 5 16 136",
+	"fourth-topic.html 1 6 64",
+	"topics 2 10 8 3 10 8",
+	"above 2 10 8 3 10 8",
+	"level 2 10 8 3 10 8",
+	"introduction.html 2 6 64",
+	"more-stuff.html 3 6 64",
+	"second 4 39 216",
+	"second-topic.html 4 6 64",
+	"third 5 39 216",
+	"third-topic.html 5 6 64"];
+skipwords = ["and,or,the,it,is,an,on,we,us,to,of,"];
+var STR_FORM_SEARCHFOR = "Search for:";
+var STR_FORM_SUBMIT_BUTTON = "Submit";
+var STR_FORM_RESULTS_PER_PAGE = "Results per page:";
+var STR_FORM_MATCH = "Match:";
+var STR_FORM_ANY_SEARCH_WORDS = "any search words";
+var STR_FORM_ALL_SEARCH_WORDS = "all search words";
+var STR_NO_QUERY = "No search query entered.";
+var STR_RESULTS_FOR = "Search results for:";
+var STR_NO_RESULTS = "No results";
+var STR_RESULT = "result";
+var STR_RESULTS = "results";
+var STR_PHRASE_CONTAINS_COMMON_WORDS = "Your search query contained too many common words to return the entire set of results available. Please try again with a more specific query for better results.";
+var STR_SKIPPED_FOLLOWING_WORDS = "The following word(s) are in the skip word list and have been omitted from your search:";
+var STR_SKIPPED_PHRASE = "Note that you can not search for exact phrases beginning with a skipped word";
+var STR_SUMMARY_NO_RESULTS_FOUND = "No results found.";
+var STR_SUMMARY_FOUND_CONTAINING_ALL_TERMS = "found containing all search terms.";
+var STR_SUMMARY_FOUND_CONTAINING_SOME_TERMS = "found containing some search terms.";
+var STR_SUMMARY_FOUND = "found.";
+var STR_PAGES_OF_RESULTS = "pages of results.";
+var STR_POSSIBLY_GET_MORE_RESULTS = "You can possibly get more results searching for";
+var STR_ANY_OF_TERMS = "any of the terms";
+var STR_DIDYOUMEAN = "Did you mean:";
+var STR_SORTEDBY_RELEVANCE = "Sorted by relevance";
+var STR_SORTBY_RELEVANCE = "Sort by relevance";
+var STR_SORTBY_DATE = "Sort by date";
+var STR_SORTEDBY_DATE = "Sorted by date &#9660;";
+var STR_RESULT_TERMS_MATCHED = "Terms matched: ";
+var STR_RESULT_SCORE = "Score: ";
+var STR_RESULT_URL = "URL:";
+var STR_RESULT_PAGES = "Result Pages:";
+var STR_RESULT_PAGES_PREVIOUS = "Previous";
+var STR_RESULT_PAGES_NEXT = "Next";
+var STR_FORM_CATEGORY = "Category:";
+var STR_FORM_CATEGORY_ALL = "All";
+var STR_FORM_DATE_FROM = "From:";
+var STR_FORM_DATE_TO = "To:";
+var STR_FORM_DATE_BUTTON = "&laquo;";
+var STR_RESULTS_IN_ALL_CATEGORIES = "in all categories";
+var STR_RESULTS_IN_CATEGORY = "in category";
+var STR_POWEREDBY = "Search powered by";
+var STR_MORETHAN = "More than";
+var STR_ALL_CATS = "all categories";
+var STR_CAT_SUMMARY = "Refine your search by category:";
+var STR_OR = "or";
+var STR_RECOMMENDED = "Recommended links";
+var STR_SORTEDBY_DATE_ASC = "Sorted by date &#9650;";
+var STR_SEARCH_TOOK = "Search took";
+var STR_SECONDS = "seconds";
+var STR_MAX_RESULTS = "You have requested more results than served per query. Please try again with a more precise query.";
---
+++ zoom_pageinfo.js
@@ -0,0 +1,12 @@
+pageinfo = [[0,0,0,0,null],
+	[0,0,0,0,null],
+	[0,0,0,0,null],
+	[0,0,0,0,null],
+	[0,0,0,0,null],
+	[0,0,0,0,null]];
+pagedata = [ ["./first-topic.html","Introduction &#62; First topic","This is the first topic.",""],
+["./fourth-topic.html","More stuff &#62; Fourth topic","This is the fourth topic.",""],
+["./introduction.html","Introduction","This is the introduction.",""],
+["./more-stuff.html","More stuff","This is more stuff.",""],
+["./second-topic.html","Introduction &#62; Second topic","This is the second topic.",""],
+["./third-topic.html","More stuff &#62; Third topic","This is the third topic.",""]];
